ffe4791b5b3593744acc0beb54f182de0ed55613
DocRock3r <docrock3r@noreply.localhost>
2025-10-21 18:26:47 +0000

    Update Scripts/USE-AIO.sh
    Signed-off-by: DocRock3r <docrock3r@noreply.localhost>

--- Scripts/USE-AIO.sh
+++ Scripts/USE-AIO.sh
@@ -60,7 +60,31 @@ case $choice in
     sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts 1>/dev/null
     echo "Done" ;;
 
-3)  echo "Updating repository..."
+3)  echo "Installing Docker..."
+    # Add Docker's official GPG key:
+    sudo apt-get update 1>/dev/null 
+    sudo apt-get install ca-certificates curl -y 1>/dev/null 
+    sudo install -m 0755 -d /etc/apt/keyrings 1>/dev/null 
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc 1>/dev/null 
+    sudo chmod a+r /etc/apt/keyrings/docker.asc 1>/dev/null 
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update 1>/dev/null 
+
+    # Install Docker
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 1>/dev/null
+    echo "Done"
+
+    #install Portianer agent
+    echo "Installing Portainer agent..."
+    sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes -v /:/host portainer/agent:2.21.5 1>/dev/null
+    echo "Done" ;;
+
+4)  echo "Updating repository..."
     sudo apt-get update 1>/dev/null
     echo "Done"
     echo "Installing updates..."
@@ -70,7 +94,7 @@ case $choice in
     sudo apt-get autoremove -y 1>/dev/null
     echo "Done" ;;
 
-4)  echo ""
+5)  echo ""
 
     #get current hostname and set it as value of old_host
     old_host=$(hostname)
@@ -95,7 +119,7 @@ case $choice in
     printf "%s\n" "Hostname changed from [$old_host] to [$new_host]"
     printf "%s\n" "Reboot your system for the change to take effect" ;;
 
-5)  echo "Exiting..."
+00)  echo "Exiting..."
     exit ;;
 
 *)  echo "Invalid option" ;;
