a9f4ba07f419035a9f5bff754c46f61578a73ed5
DocRock3r <docrock3r@noreply.localhost>
2025-11-14 15:31:40 +0000

    Update Scripts/USE-AIO.sh
    Signed-off-by: DocRock3r <docrock3r@noreply.localhost>

--- Scripts/USE-AIO.sh
+++ Scripts/USE-AIO.sh
@@ -82,7 +82,7 @@ case $choice in
 
     #install Portianer agent
     echo "Installing Portainer agent..."
-    sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes -v /:/host portainer/agent:2.21.5 1>/dev/null
+    sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes -v /:/host portainer/agent:lts 1>/dev/null
     echo "Done" ;;
 
 4)  echo "Updating repository..."
